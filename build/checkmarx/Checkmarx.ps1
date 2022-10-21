318152c3ecc53542386a7250741d4a635ab066bb
Steve Salas <salas@synopsys.com>
2022-10-21 11:26:41 -0400

    feat: Use isIncremental and token refresh

--- build/checkmarx/Checkmarx.ps1
+++ build/checkmarx/Checkmarx.ps1
@@ -19,6 +19,58 @@ $VerbosePreference = 'Continue'
 
 . $PSScriptRoot/add-in.ps1
 
+class Token {
+
+	[string]   $accessToken
+	[DateTime] $expirationTime
+
+	Token([string] $accessToken, [int] $tokenLifetimeInSeconds) {
+		$this.accessToken = $accessToken
+		$this.expirationTime = [DateTime]::Now.AddSeconds($tokenLifetimeInSeconds)
+	}
+
+	[bool] isExpired() {
+		return [DateTime]::Now -ge $this.expirationTime
+	}
+
+	[bool] isValid() {
+		return -not $this.isExpired()
+	}
+}
+
+function New-CheckmarxToken([string] $tokenUrl, [string] $checkmarxUsername, [string] $checkmarxPwd) {
+
+	# Note: The Checkmarx documentation states that this parameter must have the value specified here
+	$clientSecret = '014DF517-39D1-4453-B7B3-9930C563627C'
+
+	$tokenBody = @{
+		'username'=$checkmarxUsername
+		'password'=$checkmarxPwd
+		'grant_type'='password'
+		'scope'='sast_rest_api'
+		'client_id'='resource_owner_client'
+		'client_secret'=$clientSecret
+	}
+
+	write-verbose "Obtaining bearer token from $tokenUrl..."
+	Invoke-RestMethod -Uri $tokenUrl `
+		-Method Post `
+		-Body $tokenBody
+}
+
+function Get-CheckmarxToken([string] $tokenUrl, [int] $tokenLifetimeInSeconds, 
+	[string] $checkmarxUsername, [string] $checkmarxPwd, 
+	[Token] $previousToken) {
+
+	$token = $previousToken
+	if ($null -ne $token -and $token.isValid()) {
+		return $token
+	}
+
+	$tokenResponse = New-CheckmarxToken $tokenUrl $checkmarxUsername $checkmarxPwd
+	return new-object Token($tokenResponse.access_token, $tokenLifetimeInSeconds)
+}
+
 write-verbose "Reading scan request file ($scanRequestFilePath)..."
 $scanRequestConfig = Get-Config $scanRequestFilePath
 
@@ -38,43 +90,39 @@ Set-Tlsv12
 $checkmarxBaseUrl = $scanRequestConfig.checkmarx.baseurl
 $tokenUrl = "$checkmarxBaseUrl/cxrestapi/auth/identity/connect/token"
 
-write-verbose "Step 1: Obtaining bearer token from $tokenUrl..."
-
-# Note: The Checkmarx documentation states that this parameter must have the value specified here
-$clientSecret = '014DF517-39D1-4453-B7B3-9930C563627C'
-
-$tokenBody = @{
-	'username'=$checkmarxUsername
-	'password'=$checkmarxPassword
-	'grant_type'='password'
-	'scope'='sast_rest_api'
-	'client_id'='resource_owner_client'
-	'client_secret'=$clientSecret
+$tokenLifetimeInSeconds = 3000
+if ($null -ne $scanRequestConfig.checkmarx.tokenLifetimeInSeconds) {
+	$tokenLifetimeInSeconds = $scanRequestConfig.checkmarx.tokenLifetimeInSeconds
 }
+write-verbose "Using tokenLifetimeInSeconds $tokenLifetimeInSeconds"
 
-$tokenResponse = Invoke-RestMethod -Uri $tokenUrl `
-	-Method Post `
-	-Body $tokenBody
-
-$accessToken = $tokenResponse.access_token
-
-$authorizationHeader = @{
-	'Authorization' = "Bearer $accessToken"
+$token = $null
+$authorizationHeader = {
+	$script:token = Get-CheckmarxToken $tokenUrl $tokenLifetimeInSeconds $checkmarxUsername $checkmarxPassword $script:token
+	@{
+		'Authorization' = "Bearer $($script:token.accessToken)"
+	}
 }
 
 $inputDirectory = join-path $scanRequestConfig.request.workdirectory 'input'
 $sourcePath = (Get-ChildItem $inputDirectory | Select-Object -First 1).FullName
 
-write-verbose "Step 2: Uploading source $sourcePath..."
+write-verbose "Step: Uploading source $sourcePath..."
 $form = @{zippedSource=(Get-ChildItem $sourcePath)}
-Invoke-RestMethod "$checkmarxBaseUrl/cxrestapi/projects/$checkmarxProjectId/sourceCode/attachments" -Method Post -Form $form -Headers @{Authorization="Bearer $accessToken"}
+Invoke-RestMethod "$checkmarxBaseUrl/cxrestapi/projects/$checkmarxProjectId/sourceCode/attachments" -Method Post -Form $form -Headers (& $authorizationHeader)
 
-write-verbose 'Step 3: Starting scan...'
+write-verbose 'Step: Starting scan...'
 $startScanUrl = "$checkmarxBaseUrl/cxrestapi/sast/scans"
+
+$isIncremental = 'false'
+if ($null -ne $scanRequestConfig.scan.isIncremental) {
+	$isIncremental = $scanRequestConfig.scan.isIncremental.tostring().tolower()
+}
+write-verbose "Using isIncremental $isIncremental"
 
 $startScanBody = @{
 	'projectId' = "$checkmarxProjectId"
-	'isIncremental' = 'false'
+	'isIncremental' = $isIncremental
 	'isPublic' = 'true'
 	'forceScan' = 'true'
 	'comment' = "$checkmarxProjectId"
@@ -83,28 +131,28 @@ $startScanBody = @{
 $startScanResponse = Invoke-RestMethod -Uri $startScanUrl `
 	-Method Post `
 	-Body (ConvertTo-Json $startScanBody) `
-	-Header $authorizationHeader `
+	-Header (& $authorizationHeader) `
 	-ContentType 'application/json'
 
 $scanId = $startScanResponse.id
 
 $waitForCompletionSleepTimeInSeconds = $scanRequestConfig.scan.checkscanstatusdelay
-write-verbose "Step 4: Wait for scan to complete (using check-status delay $($waitForCompletionSleepTimeInSeconds))..."
+write-verbose "Step: Wait for scan to complete (using check-status delay $($waitForCompletionSleepTimeInSeconds))..."
 
 $waitForScanUrl = "$checkmarxBaseUrl/cxrestapi/sast/scans/$scanId"
 $waitForScanResponse = Invoke-RestMethod -Uri $waitForScanUrl `
 	-Method Get `
-	-Header $authorizationHeader
+	-Header (& $authorizationHeader)
 
 while ($waitForScanResponse.status.name -ne 'Finished') {
 	write-verbose "  Waiting for scan completion..."
 	Start-Sleep -seconds $waitForCompletionSleepTimeInSeconds
 	$waitForScanResponse = Invoke-RestMethod -Uri $waitForScanUrl `
 		-Method Get `
-		-Header $authorizationHeader
+		-Header (& $authorizationHeader)
 }
 
-write-verbose 'Step 5: Creating new XML report...'
+write-verbose 'Step: Creating new XML report...'
 $createReportUrl = "$checkmarxBaseUrl/cxrestapi/reports/sastScan"
 
 $createReportBody = @{
@@ -114,32 +162,32 @@ $createReportBody = @{
 
 $createReportResponse = Invoke-RestMethod -Uri $createReportUrl `
 	-Method Post `
-	-Header $authorizationHeader `
+	-Header (& $authorizationHeader) `
 	-Body $createReportBody
 
 $reportId = $createReportResponse.reportId
 
-write-verbose 'Step 6: Waiting for report to complete...'
+write-verbose 'Step: Waiting for report to complete...'
 $getReportStatusUrl = "$checkmarxBaseUrl/cxrestapi/reports/sastScan/$reportId/status"
 
 $getReportStatusResponse = Invoke-RestMethod -Uri $getReportStatusUrl `
 	-Method Get `
-	-Header $authorizationHeader
+	-Header (& $authorizationHeader)
 
 while ($getReportStatusResponse.status.value -ne 'Created') {
 	write-verbose "  Waiting for report completion..."
 	Start-Sleep -seconds $waitForCompletionSleepTimeInSeconds
 	$getReportStatusResponse = Invoke-RestMethod -Uri $getReportStatusUrl `
 		-Method Get `
-		-Header $authorizationHeader
+		-Header (& $authorizationHeader)
 }
 
-write-verbose 'Step 7: Fetching XML report...'
+write-verbose 'Step: Fetching XML report...'
 $fetchReportUrl = "$checkmarxBaseUrl/cxrestapi/reports/sastScan/$reportId"
 
 $fetchReportResponse = Invoke-RestMethod -Uri $fetchReportUrl `
 	-Method Get `
-	-Header $authorizationHeader
+	-Header (& $authorizationHeader)
 
 $reportOutputPath = $scanRequestConfig.request.resultfilepath
 write-verbose "Saving report to $reportOutputPath..."
